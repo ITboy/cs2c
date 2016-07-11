c13cd78f5988a20a58cf8121b3b5d10f005de6bb
prepper <prepper@126.com>
2016-07-11 10:51:43 +0800

    Create ErrorCode.mk

---
+++ ErrorCode.mk
@@ -0,0 +1,9 @@
+# This is OCF's error code list
+| error_id | error_code | error_message |
+|------------|---------------|---------------------|
+|OCF_SUCCESS | 0 | success|
+|OCF_SPR_FAIL_TO_GET_LOCK | 3201 | fail to get lock when load subscriber |
+|OCF_SPR_NO_SUBSCRIBER_FOUND | 3202 | no subscriber found when load subscriber |
+|OCF_SPR_LOAD_CLOSED_SUBSCRIBER | 3209 | the subscriber to be loaded has been closed |
+|OCF_INTERNAL_ERROR | 4001 | internal error |
+|OCF_CCR_REQNO_NOT_INCREMENT| 1  |Current CCR ccRequestNumber should be larger than the last CCR ccRequestNumber |
